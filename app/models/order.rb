896a50b0e9f77ff0058065820958c81e25df4fb0
Dave Ott <david.t.ott@gmail.com>
2024-01-31 15:05:44 -0500

    Add 2 scopes to get frequently ordered products in the last month
    Limit the results by 3

---
+++ app/models/order.rb
@@ -0,0 +1,13 @@
+class Order < ApplicationRecord
+  belongs_to :product
+
+  scope :last_month, -> { where('orders.created_at > ?', 1.month.ago) }
+  scope :frequently_ordered, -> {
+    left_joins(:product)
+       .group(:name)
+       .having('count(*) > 1')
+       .limit(3)
+       .order("count_all DESC")
+       .count
+  }
+end
---
+++ spec/models/order_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  let(:product) { Product.create(name: 'Test Product') }
+
+  describe '.last_month' do
+    let!(:old_order) { Order.create(product:, created_at: 2.months.ago) }
+    let!(:recent_order) { Order.create(product:, created_at: 2.weeks.ago) }
+
+    it 'returns orders created in the last month' do
+      expect(Order.last_month).to eq([recent_order])
+    end
+  end
+
+  describe '.frequently_ordered' do
+    let(:product_1) { Product.create(name: 'Product 1') }
+    let(:product_2) { Product.create(name: 'Product 2') }
+    let(:product_3) { Product.create(name: 'Product 3') }
+    let(:product_4) { Product.create(name: 'Product 4') }
+
+    let!(:order_1) { Order.create(product: product_1) }
+    let!(:order_2) { Order.create(product: product_1) }
+    let!(:order_3) { Order.create(product: product_1) }
+    let!(:order_4) { Order.create(product: product_2) }
+    let!(:order_5) { Order.create(product: product_2) }
+    let!(:order_6) { Order.create(product: product_2) }
+    let!(:order_7) { Order.create(product: product_3) }
+    let!(:order_8) { Order.create(product: product_3) }
+    let!(:order_9) { Order.create(product: product_2) }
+
+    it 'returns the most frequently ordered products, DESC count' do
+      expect(Order.frequently_ordered).to eq({ 'Product 2' => 4, 'Product 1' => 3, 'Product 3' => 2 })
+    end
+
+    context 'ordered in the last month' do
+      let!(:order_10) { Order.create(product: product_4, created_at: 2.months.ago) }
+      let!(:order_11) { Order.create(product: product_4, created_at: 3.months.ago) }
+
+      it 'returns the most frequently ordered products in the last month, DESC count' do
+        expect(Order.last_month.frequently_ordered).to eq({ 'Product 2' => 4, 'Product 1' => 3, 'Product 3' => 2 })
+      end
+    end
+  end
+end
