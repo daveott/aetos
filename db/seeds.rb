d7d1989f28a726f1fca958b6ff5b1912b8fb70ac
Dave Ott <david.t.ott@gmail.com>
2024-01-31 15:05:14 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Product.create(name: 'fridge')
+Product.create(name: 'ball')
+Product.create(name: 'mouse')
+Product.create(name: 'keyboard')
+Product.create(name: 'lightsaber')
+Product.create(name: 'oven mitt')
+Product.create(name: 'ipod')
+Product.create(name: 'tea pot')
+
+product_ids = Product.all.pluck(:id)
+200.times do
+  Order.create(product_id: product_ids.sample, order_date: rand(1..365).days.ago.to_s)
+end
